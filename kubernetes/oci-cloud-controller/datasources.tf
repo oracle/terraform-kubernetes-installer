13dee6f17a63e59988345997caf723c1c5c9f9f9
Garth Bushell <garth.bushell@oracle.com>
2018-01-11 17:49:42 +0000

    Do some magic to work around terraform ternary not being lazy (https://github.com/hashicorp/terraform/issues/2831#issuecomment-298751019)

--- kubernetes/oci-cloud-controller/datasources.tf
+++ kubernetes/oci-cloud-controller/datasources.tf
@@ -7,9 +7,9 @@ data "template_file" "oci-cloud-controller-secret" {
     compartment = "${var.compartment_ocid}"
     
     # if cloud_controller_user_ocid is empty string then generate a user/group/policy and use it here
-    fingerprint = "${var.cloud_controller_user_ocid==""? oci_identity_api_key.cloud_controller_key_assoc.fingerprint : var.cloud_controller_user_fingerprint }"
-    user        = "${var.cloud_controller_user_ocid==""? oci_identity_user.cloud_controller_user.id : var.cloud_controller_user_ocid }"
-    key         = "${var.cloud_controller_user_ocid==""? jsonencode(tls_private_key.cloud_controller_user_key.private_key_pem) : jsonencode(file(var.cloud_controller_user_private_key_path))}"
+    fingerprint = "${var.cloud_controller_user_ocid=="" ? join("", oci_identity_api_key.cloud_controller_key_assoc.*.fingerprint) : var.cloud_controller_user_fingerprint }"
+    user        = "${var.cloud_controller_user_ocid=="" ? join("", oci_identity_user.cloud_controller_user.*.id) : var.cloud_controller_user_ocid }"
+    key         = "${var.cloud_controller_user_ocid=="" ? jsonencode(join("", tls_private_key.cloud_controller_user_key.*.private_key_pem)) : jsonencode(file(var.cloud_controller_user_private_key_path))}"
     
     subnet1 = "${var.subnet1}"
     subnet2 = "${var.subnet1}"
