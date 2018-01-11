d454b5ed88371a229d6326e9b9e22cfeb5de5af8
Garth Bushell <garth.bushell@oracle.com>
2018-01-11 14:23:49 +0000

    Fix types, restrict policy to compartments

--- kubernetes/oci-cloud-controller/user.tf
+++ kubernetes/oci-cloud-controller/user.tf
@@ -5,12 +5,12 @@ resource "tls_private_key" "cloud_controller_user_key" {
 
 resource "oci_identity_group" "cloud_controller_group" {
     name = "${var.label_prefix}cloud_controller_group"
-    description = "Terraform created group for ocii cloud controller"
+    description = "Terraform created group for OCI Cloud controller manager"
 }
 
 resource "oci_identity_user" "cloud_controller_user" {
   name = "${var.label_prefix}cloud_controller_user"
-  description = "Terraform created user for ocii cloud controller"
+  description = "Terraform created user for OCI Cloud controller manager"
 }
 
 resource "oci_identity_api_key" "cloud_controller_key_assoc" {
@@ -26,16 +26,16 @@ resource "oci_identity_user_group_membership" "cloud_controller_user_group_assoc
 
 resource "oci_identity_policy" "cloud_controller_policy" {
   depends_on = ["oci_identity_group.cloud_controller_group"]
-  compartment_id = "${var.tenancy}"
+  compartment_id = "${var.compartment_ocid}"
   name = "${var.label_prefix}cloud_controller_policy"
   description = "${var.label_prefix}cloud_controller_group policy"
   statements = [
-    "Allow group id ${oci_identity_group.cloud_controller_group.id} to manage load-balancers in tenancy", #Restrict to compartment?
-    "Allow group id ${oci_identity_group.cloud_controller_group.id} to use security-lists in tenancy",
-    "Allow group id ${oci_identity_group.cloud_controller_group.id} to read instances in tenancy",
-    "Allow group id ${oci_identity_group.cloud_controller_group.id} to read subnets in tenancy",
-    "Allow group id ${oci_identity_group.cloud_controller_group.id} to read vnics in tenancy",
-    "Allow group id ${oci_identity_group.cloud_controller_group.id} to read vnic-attachments in tenancy",
+    "Allow group id ${oci_identity_group.cloud_controller_group.id} to manage load-balancers in compartment id ${var.compartment_ocid}", 
+    "Allow group id ${oci_identity_group.cloud_controller_group.id} to use security-lists in compartment id ${var.compartment_ocid}",
+    "Allow group id ${oci_identity_group.cloud_controller_group.id} to read instances in compartment id ${var.compartment_ocid}",
+    "Allow group id ${oci_identity_group.cloud_controller_group.id} to read subnets in compartment id ${var.compartment_ocid}",
+    "Allow group id ${oci_identity_group.cloud_controller_group.id} to read vnics in compartment id ${var.compartment_ocid}",
+    "Allow group id ${oci_identity_group.cloud_controller_group.id} to read vnic-attachments in compartment id ${var.compartment_ocid}",
   ]
 }
 
