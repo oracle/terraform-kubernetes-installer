c0607efbb0217dd3ecbb92e6fc6481e685638876
Garth Bushell <garth.bushell@oracle.com>
2018-01-17 17:07:27 +0000

    set the name correctly

--- network/vcn/securitylists.tf
+++ network/vcn/securitylists.tf
@@ -239,7 +239,7 @@ resource "oci_core_security_list" "PublicSecurityList" {
 
 resource "oci_core_security_list" "K8SCCMLBSubnet" {
   compartment_id = "${var.compartment_ocid}"
-  display_name   = "${var.label_prefix}etcd_security_list"
+  display_name   = "${var.label_prefix}k8sccmlbsubnet"
   vcn_id         = "${oci_core_virtual_network.CompleteVCN.id}"
   egress_security_rules = [{
     protocol    = "all"
